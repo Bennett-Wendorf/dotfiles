22ad90487ce268761f1af0fc707a64b848ffeb25
Bennett-Wendorf <bennettwendorf@gmail.com>
2020-06-30 10:59:49 -0500

    Updated fish, polybar, and i3 configs. No major changes.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
 alias dotfiles='/usr/bin/git --git-dir=/home/bennett/dotfiles --work-tree=/home/bennett'
+alias clock='watch -n .5 "lscpu | grep MHz"'
 
 function fish_greeting
 	fortune -a computers
