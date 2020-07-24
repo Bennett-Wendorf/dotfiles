d21884f62dc405f209f7552319c8b5ae121af069
Bennett-Wendorf <bennettwendorf@gmail.com>
2020-07-23 20:11:33 -0500

    General housekeeping.

--- .bashrc
+++ .bashrc
@@ -9,3 +9,7 @@
 alias ls='ls --color=auto'
 PS1='[\u@\h \W]\$ '
 alias dotfiles='/usr/bin/git --git-dir=/home/bennett/dotfiles --work-tree=/home/bennett'
+
+# Install Ruby Gems to ~/gems
+export GEM_HOME="$HOME/ruby_gems"
+export PATH="$PATH:$HOME/ruby_gems/bin"
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,3 +4,17 @@ alias clock='watch -n .5 "lscpu | grep MHz"'
 function fish_greeting
 	fortune -a computers
 end
+# Install Ruby Gems to ~/ruby_gems
+export GEM_HOME="$HOME/ruby_gems"
+export PATH="$HOME/ruby_gems/bin:$PATH"
+export PATH="$HOME/.gem/ruby/2.7.0/bin:$PATH"
+
+# Import colorscheme from 'wal' asynchronously
+# &   # Run the process in the background
+cat ~/.cache/wal/sequences &
+
+
+function pywal-pacwall
+    wal -i "/home/bennett/Desktop/blue-black-cirguit.jpg" -b "#303030" -n
+    pacwall -W
+end
