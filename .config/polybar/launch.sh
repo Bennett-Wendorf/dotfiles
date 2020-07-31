c833b3d9f7c9fab9fc68974ca5eca48e940fd48c
Bennett-Wendorf <bennettwendorf@gmail.com>
2020-07-30 20:10:39 -0500

    Added playback controls using playerctl.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -4,7 +4,9 @@
 killall -q polybar
 
 # Wait until the processes have been shut down
-#while pgrep -u $UID -x polybar >/dv/null; do sleep 1; done
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+#~/scripts/spotify/launchlistener.sh
 
 # Launch Polybar, using default config location ~/.config/polybar/config
 polybar top &
