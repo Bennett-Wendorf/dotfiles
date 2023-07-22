a28f6fb2a7ef2a4316db3be274863b33acd37cf0
Bennett-Wendorf <bennettwendorf@gmail.com>
2023-07-21 22:45:33 -0500

    Update pacman hooks to clean yay cache as well
    Signed-off-by: Bennett-Wendorf <bennettwendorf@gmail.com>

---
+++ scripts/pacman_hooks_helpers/pkgclean.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+yaycache="$(find /home/bennett/.cache/yay/ -maxdepth 1 -type d | tail -n +2 | awk '{ printf "-c %s ",$1 }')"
+
+/usr/bin/paccache -rvk1 -c /var/cache/pacman/pkg $'$yaycache'
