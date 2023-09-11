2d6757ddb938b3118725ed6a8469ff84794984f7
Bennett-Wendorf <bennettwendorf@gmail.com>
2023-09-11 17:23:16 -0500

    Rewrote get_curr_screen.sh to better handle more configurations
    Signed-off-by: Bennett-Wendorf <bennettwendorf@gmail.com>

--- .config/eww/scripts/get_curr_screen.sh
+++ .config/eww/scripts/get_curr_screen.sh
@@ -1,12 +1,35 @@
-#!/usr/bin/env bash
-set -euo pipefail
+#!/bin/bash
 
-screen_1_width=$(xrandr | grep '*' | awk '{print $1}' | head -n 1 | awk 'BEGIN { FS = "x" } ; { print $1 }')
+connected_screens=$(xrandr | grep -w 'connected')
 
-mouse_x=$(xdotool getmouselocation --shell | grep "X=" | awk 'BEGIN { FS = "=" } ; { print $2 }')
+screen_info=()
 
-if [[ $mouse_x -gt screen_1_width ]]; then
-    printf "1"
-else 
-    printf "0"
-fi
+while IFS= read -r line; do
+    dim=$(echo "$line" | awk '{ print $3 }')
+    if [ "$dim" == "primary" ]; then
+        dim=$(echo "$line" | awk '{ print $4 }')
+    fi
+    screen_info+=("$dim")
+done <<< "$connected_screens"
+
+window_geometry=$(xdotool getwindowgeometry --shell "$(xdotool getwindowfocus)")
+
+window_x=$(echo "$window_geometry" | awk -F= '/X/ { print $2 }')
+window_y=$(echo "$window_geometry" | awk -F= '/Y/ { print $2 }')
+
+focused_screen_index=-1
+for i in "${!screen_info[@]}"; do
+    screen_dimensions="${screen_info[$i]}"
+    screen_x=$(echo "$screen_dimensions" | cut -d'+' -f2)
+    screen_y=$(echo "$screen_dimensions" | cut -d'+' -f3)
+    screen_width=$(echo "$screen_dimensions" | cut -dx -f1)
+    screen_height=$(echo "$screen_dimensions" | cut -dx -f2 | cut -d'+' -f1)
+
+    if [ "$window_x" -ge "$screen_x" ] && [ "$window_x" -lt "$((screen_x + screen_width))" ] &&
+       [ "$window_y" -ge "$screen_y" ] && [ "$window_y" -lt "$((screen_y + screen_height))" ]; then
+        focused_screen_index="$i"
+        break
+    fi
+done
+
+printf "$focused_screen_index"
