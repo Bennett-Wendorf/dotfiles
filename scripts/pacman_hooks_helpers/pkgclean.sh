f247a833d544678c106b57c1c38d5d2ccad92f99
Bennett-Wendorf <bennettwendorf@gmail.com>
2024-03-09 22:48:19 -0600

    Fixed pacman and yay cache cleaner hook scripts
    Signed-off-by: Bennett-Wendorf <bennettwendorf@gmail.com>

--- scripts/pacman_hooks_helpers/pkgclean.sh
+++ scripts/pacman_hooks_helpers/pkgclean.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 yaycache="$(find /home/bennett/.cache/yay/ -maxdepth 1 -type d | tail -n +2 | awk '{ printf "-c %s ",$1 }')"
 
-/usr/bin/paccache -rvk1 -c /var/cache/pacman/pkg $'$yaycache'
+/usr/bin/paccache -rvk1 -c /var/cache/pacman/pkg $yaycache
--- scripts/pacman_hooks_helpers/pkgpurge.sh
+++ scripts/pacman_hooks_helpers/pkgpurge.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 yaycache="$(find /home/bennett/.cache/yay/ -maxdepth 1 -type d | tail -n +2 | awk '{ printf "-c %s ",$1 }')"
 
-/usr/bin/paccache -rvuk0 -c /var/cache/pacman/pkg $'$yaycache'
+/usr/bin/paccache -rvuk0 -c /var/cache/pacman/pkg $yaycache
