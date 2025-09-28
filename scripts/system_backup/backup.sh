752890782f98758475916beb70b5b019158ce185
Bennett-Wendorf <bennettwendorf@gmail.com>
2025-09-28 11:32:02 -0500

    Changed pbs datastore
    Signed-off-by: Bennett-Wendorf <bennettwendorf@gmail.com>

--- scripts/system_backup/backup.sh
+++ scripts/system_backup/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-proxmox-backup-client backup root.pxar:/ --exclude=/home/bennett/files --exclude=/home/bennett/.cache --repository 'bwarch@pbs!bwarchtoken@10.66.1.25:data' --ns misc
+proxmox-backup-client backup root.pxar:/ --exclude=/home/bennett/files --exclude=/home/bennett/.cache --repository 'bwarch@pbs!bwarchtoken@10.66.1.25:local' --ns misc
 
