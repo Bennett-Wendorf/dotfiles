e5e3597ac9e6b49721deea80627e43940ea5ef6a
Bennett-Wendorf <bennettwendorf@gmail.com>
2026-06-11 14:24:48 -0500

    Exclude swapfile from backups
    Signed-off-by: Bennett-Wendorf <bennettwendorf@gmail.com>

--- scripts/system_backup/backup.sh
+++ scripts/system_backup/backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-proxmox-backup-client backup root.pxar:/ --exclude=/home/bennett/files --exclude=/home/bennett/.cache --repository 'bwarch@pbs!bwarchtoken@10.66.1.25:local' --ns misc
+proxmox-backup-client backup root.pxar:/ --exclude=/home/bennett/files --exclude=/home/bennett/.cache --exclude=/swapfile --repository 'bwarch@pbs!bwarchtoken@10.66.1.25:local' --ns misc
 
